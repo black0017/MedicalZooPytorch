105fd2f56b727daf63447a25eef405dfdd589035
Adaloglou Nikolaos <black.adaloglou@gmail.com>
2020-04-10 21:09:02 +0200

    add vae-resnet loss

--- src/utils/general.py
+++ src/utils/general.py
@@ -69,3 +69,17 @@ def create_stats_files(path):
     train_f = open(os.path.join(path, 'train.csv'), 'w')
     val_f = open(os.path.join(path, 'val.csv'), 'w')
     return train_f, val_f
+
+
+
+# Reconstruction + KL divergence losses summed over all elements and batch
+def loss_vae(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return BCE, KLD
