92b9fd25b789c0bdcc1560950e56ccf99bd05bf9
SergiosKar <sergioskarag@gmail.com>
2020-07-25 14:31:32 +0100

    include all packages in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import pathlib
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -22,7 +22,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["medzoo"],
+    packages=find_packages(exclude=("tests")),
     include_package_data=True,
     entry_points={
         "console_scripts": [
