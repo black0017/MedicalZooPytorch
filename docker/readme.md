71fbc3239e5877093d81b1e865deec8cfd5b20c3
SergiosKar <sergioskarag@gmail.com>
2020-07-26 09:28:37 +0100

    remove docker folder

--- docker/readme.md
+++
@@ -1,9 +0,0 @@
-## Commands for docker setup
-
-docker build -t enviroment:latest .
-
-
-docker images
-
-
-docker run -it enviroment
--- setup.py
+++ setup.py
@@ -63,9 +63,7 @@ AUTHOR_EMAIL = ""
 LICENSE = "MIT"
 DEPENDENCY_LINKS = []
 REQUIREMENTS = (fetch_requirements())
-# Need to exclude folders in tests as well so as they don't create an extra package
-# If something from tools is regularly used consider converting it into a cli command
-EXCLUDES = ("contribute", "docker", "datasets", "examples", "figures", "installation", "manual", "notebooks", "results", "runs", "tests" )
+EXCLUDES = ("contribute", "docker", "datasets", "examples", "figures", "installation", "manual", "notebooks", "results", "runs", "tests")
 EXT_MODULES = []
 
 if __name__ == "__main__":
